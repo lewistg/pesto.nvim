55e17faa1e050183d082678dbcdbf1254d77b849
Ty Lewis <tyglewis@gmail.com>
2025-06-27 19:40:48 -0600

    refactor: extract function to get bazel-runner context

--- lua/pesto/runner/runner.lua
+++ lua/pesto/runner/runner.lua
@@ -1,3 +1,7 @@
+local M = {}
+
+local bazel_repo = require("pesto.bazel").repo
+
 ---@class RunBazelContext
 ---@field workspace_dir string
 ---@field package_dir string|nil
@@ -7,3 +11,19 @@
 ---@field context RunBazelContext
 
 ---@alias RunBazelFn fun(RunBazelOpts)
+
+---@return RunBazelContext
+function M.get_run_bazel_context()
+	local build_file = bazel_repo.find_build_file()
+	local build_dir = vim.fs.dirname(build_file)
+
+	local workspace_marker_file = bazel_repo.find_project_root_marker_file()
+	local workspace_dir = vim.fs.dirname(workspace_marker_file)
+
+	return {
+		workspace_dir = workspace_dir,
+		package_dir = build_dir,
+	}
+end
+
+return M
