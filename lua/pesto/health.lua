2b9c7728970cc153cf967bd5a485483280e45f88
Ty Lewis <tyglewis@gmail.com>
2026-03-07 18:52:12 -0700

    fix: make health check compatible with 0.11.0

--- lua/pesto/health.lua
+++ lua/pesto/health.lua
@@ -1,10 +1,16 @@
 local M = {}
 
 function M.check()
-	vim.health.report_start("pesto.nvim")
+	vim.health.start("pesto.nvim")
+
+	if vim.version.cmp(vim.version(), { major = 0, minor = 11, patch = 0 }) >= 0 then
+		vim.health.ok("Neovim >= 0.11.0")
+	else
+		vim.health.error("Neovim >= 0.11.0 required")
+	end
 
 	local logger = require("pesto.logger")
-	vim.health.report_info(string.format("Log file: %s", logger.log_path))
+	vim.health.info(string.format("Log file: %s", logger.log_path))
 end
 
 return M
