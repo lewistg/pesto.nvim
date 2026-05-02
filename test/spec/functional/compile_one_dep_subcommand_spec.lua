28dd4f7cc7cac67332854583b87ecc1d4cbc7e35
Ty Lewis <tyglewis@gmail.com>
2026-05-02 16:39:04 -0600

    refactor: extract build window test helper

--- test/spec/functional/compile_one_dep_subcommand_spec.lua
+++ test/spec/functional/compile_one_dep_subcommand_spec.lua
@@ -16,33 +16,19 @@ describe('open BUILD file subcommands', function()
   ---@type pesto.FunctionalTestHelper
   local functional_test_helper
 
+  ---@type pesto.BuildWindowTestHelper
+  local build_window_test_helper
+
   before_each(function()
     nvim_chan = vim.fn.jobstart({ 'nvim', '--embed', '--headless' }, job_opts)
     functional_test_helper = require('pesto.test.functional_test_helper'):new(nvim_chan)
+    build_window_test_helper =
+      require('pesto.test.build_window_test_helper'):new(nvim_chan, functional_test_helper)
   end)
 
   after_each(function()
     vim.fn.jobstop(nvim_chan)
   end)
-
-  local function verify_compile_one_dep()
-    -- There should be two windows open in the current tab
-    local curr_tab_page_nr = vim.rpcrequest(nvim_chan, 'nvim_call_function', 'tabpagenr', { '$' })
-    local tab_info =
-      vim.rpcrequest(nvim_chan, 'nvim_call_function', 'gettabinfo', { curr_tab_page_nr })
-
-    -- There should be two windows open now
-    assert.are.same(2, #tab_info[1].windows)
-
-    --- One of the windows should be the build window
-    local build_win_ids = functional_test_helper:find_build_windows()
-    assert.are.equal(1, #build_win_ids)
-
-    local wait_status = vim.fn.wait(10 * 1000, function()
-      return functional_test_helper:get_build_exit_code() == 0
-    end)
-    assert.are.equal(0, wait_status)
-  end
 
   it('compile-one-dep builds some dependency for the currently open source file', function()
     vim.rpcrequest(nvim_chan, 'nvim_cmd', { cmd = 'edit', args = { 'hello-world/main.c' } }, {})
@@ -50,7 +36,7 @@ describe('open BUILD file subcommands', function()
     local subcommand = { 'compile-one-dep' }
     vim.rpcrequest(nvim_chan, 'nvim_cmd', { cmd = 'Pesto', args = subcommand }, {})
 
-    verify_compile_one_dep()
+    build_window_test_helper:verify_build_window_opens()
   end)
 
   it(
@@ -77,7 +63,7 @@ describe('open BUILD file subcommands', function()
       local subcommand = { 'compile-one-dep' }
       vim.rpcrequest(nvim_chan, 'nvim_cmd', { cmd = 'Pesto', args = subcommand }, {})
 
-      verify_compile_one_dep()
+      build_window_test_helper:verify_build_window_opens()
     end
   )
 end)
