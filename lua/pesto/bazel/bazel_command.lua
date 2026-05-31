599307217cb9d7a7a4a93fce4b24c11dedf3fd29
Ty Lewis <tyglewis@gmail.com>
2026-05-31 07:32:36 -0600

    feat: add utility to parse bazel command line options

--- lua/pesto/bazel/bazel_command.lua
+++ lua/pesto/bazel/bazel_command.lua
@@ -1,5 +1,19 @@
 local M = {}
 
+---@class pesto.OptionInjectionResult
+---@field existing_option_value string|nil
+---@field injected_option_value string|nil
+
+---@class pesto.FindBazelOptionResult
+---@field name string
+---@field value string|nil
+
+---@class pesto.BazelOptionSpec
+---@field long_name string
+---@field short_name string|nil
+---@field has_value boolean|nil
+---@field is_boolean boolean|nil
+
 ---@type string
 local BUILD_EVENT_JSON_FILE_OPTION = '--build_event_json_file'
 
@@ -22,23 +36,125 @@ local function find_bazel_subcommand_index(bazel_command)
 end
 
 ---@param bazel_command string[]
----@param temp_bep_file fun(): string Lazy value of the temp BEP file to use if needed
-function M.inject_bep_option(bazel_command, temp_bep_file)
+---@param option_spec pesto.BazelOptionSpec
+---@return pesto.FindBazelOptionResult|nil
+function M.find_option(bazel_command, option_spec)
+  ---@param part_index number
+  ---@param name string
+  ---@return {name: string, value: string|nil}|nil
+  local function parse_option(part_index, name)
+    ---@type string
+    local pattern
+    if name:len() > 1 then
+      pattern = '%-%-' .. name
+    elseif name:len() == 1 then
+      pattern = '%-' .. name
+    else
+      error(string.format('invalid option name: %s', name))
+    end
+
+    if not string.match(bazel_command[part_index], pattern) then
+      return
+    end
+    local ret = {
+      name = name,
+    }
+    if option_spec.has_value then
+      ret.value = bazel_command[part_index + 1]
+    end
+    return ret
+  end
+
+  ---@param part_index number
+  ---@param name string
+  ---@return {name: string, value: string|nil}|nil
+  local function parse_option_with_eq(part_index, name)
+    if not option_spec.has_value then
+      return
+    end
+
+    ---@type string
+    local pattern
+    if name:len() > 1 then
+      pattern = '%-%-' .. name .. '=(.+)'
+    elseif name:len() == 1 then
+      pattern = '%-' .. name .. '=(.+)'
+    else
+      error(string.format('invalid option name: %s', name))
+    end
+
+    local _, value = string.match(bazel_command[part_index], pattern)
+    if not value then
+      return
+    end
+    return {
+      name = name,
+      value = value,
+    }
+  end
+
+  ---@param part_index number
+  ---@param name string
+  ---@return {name: string, value: string|nil}|nil
+  local function parse_no_option(part_index, name)
+    if not option_spec.is_boolean then
+      return
+    end
+
+    ---@type string
+    local pattern
+    if name:len() > 1 then
+      pattern = '%-%-no' .. name
+    else
+      error(string.format('invalid option name: %s', name))
+    end
+
+    if string.match(bazel_command[part_index], pattern) then
+      return {
+        name = name,
+        value = 'no',
+      }
+    end
+  end
+
+  ---@type {name: string, value: string|nil}|nil
+  local ret
+  for i, _ in ipairs(bazel_command) do
+    ret = parse_option(i, option_spec.long_name)
+      or (option_spec.short_name and parse_option(i, option_spec.short_name))
+    if not ret and (option_spec.has_value or option_spec.is_boolean) then
+      ret = parse_option_with_eq(i, option_spec.long_name)
+        or (option_spec.short_name and parse_option_with_eq(i, option_spec.short_name))
+    end
+    if not ret and option_spec.is_boolean then
+      ret = parse_no_option(i, option_spec.long_name)
+    end
+    if ret then
+      break
+    end
+  end
+  return ret
+end
+
+---@param bazel_command string[]
+---@param bep_file fun(): string Lazy value of the temp BEP file to use if needed
+---@return pesto.OptionInjectionResult
+function M.inject_bep_option(bazel_command, bep_file)
   local subcommand_index = find_bazel_subcommand_index(bazel_command)
   if subcommand_index == nil then
-    return
+    return {}
   end
 
   for _, arg in ipairs(bazel_command) do
     if vim.startswith(arg, BUILD_EVENT_JSON_FILE_OPTION) then
       -- Do not override this option if it's already defined by the user
-      return
+      return {}
     end
   end
 
-  local bep_file = temp_bep_file()
+  local _bep_file = bep_file()
   table.insert(bazel_command, subcommand_index + 1, BUILD_EVENT_JSON_FILE_OPTION)
-  table.insert(bazel_command, subcommand_index + 2, bep_file)
+  table.insert(bazel_command, subcommand_index + 2, _bep_file)
 end
 
 ---@param bazel_command string[]
--- test/spec/unit/bazel/bazel_command_spec.lua
+++ test/spec/unit/bazel/bazel_command_spec.lua
@@ -1,10 +1,60 @@
+describe('find_option', function()
+  ---@type pesto.BazelOptionSpec
+  local async_option_spec = {
+    long_name = 'async',
+    is_boolean = true,
+  }
+  local keep_going_option_spec = {
+    long_name = 'keep_going',
+    short_name = 'k',
+    is_boolean = true,
+  }
+
+  ---@type {name: string, bazel_command: string[], option_spec: pesto.BazelOptionSpec, expected_result: pesto.FindBazelOptionResult|nil}[]
+  local test_cases = {
+    {
+      name = 'simple binary',
+      bazel_command = { 'bazel', 'clean', '--async' },
+      option_spec = async_option_spec,
+      expected_result = {
+        name = 'async',
+      },
+    },
+    {
+      name = "simple binary prefixed with 'no'",
+      bazel_command = { 'bazel', 'clean', '--noasync' },
+      option_spec = async_option_spec,
+      expected_result = {
+        name = 'async',
+        value = 'no',
+      },
+    },
+    {
+      name = 'simple short',
+      bazel_command = { 'bazel', 'build', '-k' },
+      option_spec = keep_going_option_spec,
+      expected_result = {
+        name = 'k',
+      },
+    },
+  }
+
+  for _, test_case in ipairs(test_cases) do
+    it(string.format('correctly finds command (test case: %s)', test_case.name), function()
+      local bazel_command = require('pesto.bazel.bazel_command')
+      local result = bazel_command.find_option(test_case.bazel_command, test_case.option_spec)
+      assert.are.same(test_case.expected_result, result)
+    end)
+  end
+end)
+
 describe('inject_bep_option', function()
   local temp_file = '/tmp/pesto.nvim/temp-123-bep.json'
   local lazy_temp_file = function()
     return temp_file
   end
 
-  ---@type {bazel_command: string[], expected_bazel_command: string[]}
+  ---@type {bazel_command: string[], expected_bazel_command: string[]}[]
   local test_cases = {
     {
       bazel_command = { 'bazel' },
@@ -57,8 +107,8 @@ describe('inject_bep_option', function()
   for _, test_case in ipairs(test_cases) do
     local bazel_command_str = table.concat(test_case.bazel_command, ' ')
     it(string.format('handles bazel command: %s', bazel_command_str), function()
-      local bazel_build_event_util = require('pesto.bazel.bazel_command')
-      bazel_build_event_util.inject_bep_option(test_case.bazel_command, lazy_temp_file)
+      local bazel_command = require('pesto.bazel.bazel_command')
+      bazel_command.inject_bep_option(test_case.bazel_command, lazy_temp_file)
       assert.are.same(test_case.expected_bazel_command, test_case.bazel_command)
     end)
   end
