c720b32c81342a41051f8d0f2b09cb8f4c3e95e9
Ty Lewis <tyglewis@gmail.com>
2026-06-28 22:55:18 -0600

    feat: add tests for test subcommand

---
+++ test/spec/functional/bazel_repo_fixture/java/src/test/java/com/example/ExampleTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExampleTest {
+
+    @Test
+    void addition() {
+        assertEquals(2, 2);
+    }
+}
